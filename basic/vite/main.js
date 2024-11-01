e3c73cf21884d303ea9567840519b0a46c2b5605
ReeFLk <reeflk8@gmail.com>
2024-11-01 20:51:36 +0100

    feat: :sparkles: Adding controls
    You con choose your point of view using your mouse

--- basic/vite/main.js
+++ basic/vite/main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const canvas = document.querySelector("canvas");
 
@@ -13,11 +14,11 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 );
-camera.position.z = 10;
+camera.position.z = 20;
 
 //3. Object
 const dodecahedronGeometry = new THREE.DodecahedronGeometry();
-const material = new THREE.MeshBasicMaterial({
+const material = new THREE.MeshLambertMaterial({
     color: 0x468568,
     emissive: 0x468568,
 });
@@ -42,15 +43,26 @@ scene.add(light);
 //5. Set up the renderer
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.pixelRatio = window.devicePixelRatio;
 
-//6. Animate the scene
+// 6. Add controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
+
+//7. Animate the scene
 function animate() {
     requestAnimationFrame(animate);
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+
+    dodecahedron.rotation.x += 0.01;
+    dodecahedron.rotation.y += 0.01;
+
+    controls.update();
+
     renderer.render(scene, camera);
 }
-
-
 animate();
