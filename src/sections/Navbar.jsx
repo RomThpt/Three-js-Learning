a0ef72ed96834f68fc2622cefc57d89071c1a2be
ReeFLk <reeflk8@gmail.com>
2024-11-02 11:39:53 +0100

    feat: :rocket: Creating portfolio

---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <header className="text-white fixed top-0 left-0 right-0 z-50 bg-black/90">
+            <div className="max-w-7xl mx-auto">
+                <div className="flex justify-between items-center py-5 mx-auto c-space">
+                    <a
+                        href="/"
+                        className="text-neutral-400 font-bold text-xl hover:text-white transition-colors"
+                    >
+                        Romain
+                    </a>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Navbar;
